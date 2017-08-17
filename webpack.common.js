2c1c43e7908a0c54dd6553dbb9670d28e8482167
DavidMBP <steadypathapp@gmail.com>
2017-08-17 12:22:43 -0500

    Created the common webpack config
    This file is the base config that all other webpack
    configs files will need

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack')
+var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  resolve: {
+    modules: ['node_modules', 'src'],
+    extensions: ['.ts', '.tsx', '.js', '.scss']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.html?$/,
+        use: {
+          loader: 'file-loader',
+          options: {name: '[name].[ext]'}
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css")
+  ]
+}
