c7d0ef13dfa302f67810e8e3b7b662065548537b
DavidMBP <steadypathapp@gmail.com>
2017-08-18 11:51:23 -0500

    Created the entry file for the bundle

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Layout from 'components/Layout'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { reducer } from 'reducer'
+import { INITIAL_STATE } from 'types'
+import 'styles'
+
+
+
+const store = createStore(reducer, INITIAL_STATE)
+
+ReactDOM.render(
+	<Provider store={store}><Layout/></Provider>,
+	document.getElementById('app')
+)
