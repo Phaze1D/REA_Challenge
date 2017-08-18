b1b8c7ab94346f8d924c077f33466cca23f632d1
DavidMBP <steadypathapp@gmail.com>
2017-08-17 20:39:35 -0500

    Changed entry namd

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ var path = require('path')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: './src/app.tsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
