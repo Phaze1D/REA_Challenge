aab0c9f88d71cac3960ceb55b81b58c10712ed26
DavidMBP <steadypathapp@gmail.com>
2017-08-17 20:33:59 -0500

    Improved jsdoc

--- src/reducer.ts
+++ src/reducer.ts
@@ -12,44 +12,59 @@ export interface IState extends Map<string, Set<string> | Map<string, IProperty>
 export const INITIAL_STATE = Map<string, Set<string> | Map<string, IProperty>>()
 
 
-
-
+/**
+* Redux Reducer function
+* @param {IState} state
+* @param {IAction} action
+* @return {IState} newState
+*/
 export function reducer(state: IState=INITIAL_STATE, action: IAction): IState {
-
   if(action.type === LOAD_DATA){
     return loadDataHandler(state, action)
   }
-
   if(action.type === ADD_PROPERTY){
     return addPropertyHandler(state, action)
   }
-
   if(action.type === REMOVE_PROPERTY){
     return removePropertyHandler(state, action)
   }
-
   return state
 }
 
-
+/**
+* Handles the loadData action by merging maps
+* @param {IState} oldState
+* @param {IAction} action
+* @return {IState} newState
+*/
 function loadDataHandler(oldState, action) {
   return oldState.merge(action.payload)
 }
 
-
+/**
+* Handles addProperty action by adding a new id to
+* the saved list if the properties map has the id
+* @param {IState} oldState
+* @param {IAction} action
+* @return {IState} newState
+*/
 function addPropertyHandler(oldState, action) {
   if(oldState.hasIn(['properties', action.payload.id])){
     return oldState.update('saved', set => set.add(action.payload.id))
   }
-
   return oldState
 }
 
-
+/**
+* Handles removeProperty action by removing a new id from
+* the saved list if the properties map has the id
+* @param {IState} oldState
+* @param {IAction} action
+* @return {IState} newState
+*/
 function removePropertyHandler(oldState, action){
   if(oldState.hasIn(['properties', action.payload.id])){
     return oldState.update('saved', set => set.delete(action.payload.id))
   }
-
   return oldState
 }
